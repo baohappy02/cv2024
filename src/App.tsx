07bd0b3d075ff83140e8007b13ca694a51e8ee00
lq.bao <lq.bao@tego.global>
2024-12-26 19:57:34 +0700

    Update second exp

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import "./app.scss";
 
 import jsPDF from "jspdf";
-import { toCanvas, toPng } from "html-to-image";
+import { toCanvas } from "html-to-image";
 import LinkedinLogo from "./assets/LinkedinLogo";
 import HomeLogo from "./assets/HomeLogo";
 import EmailLogo from "./assets/EmailLogo";
 import { useCallback, useRef, useState, memo } from "react";
-import { email, highlightedSkills, linkIn, name, role, skills } from "./const";
+import {
+  email,
+  highlightedSkills,
+  jobData,
+  linkIn,
+  name,
+  role,
+  skills,
+} from "./const";
 import { cleanLinkedInUrl } from "./utils";
 
 const Resume = () => {
@@ -158,66 +166,23 @@ const Resume = () => {
 
           <section className="experience">
             <h2>Experience</h2>
-            <div className="job">
-              <h3>Comprehensive Management Center</h3>
-              <p className="location-date">
-                Thu Duc, HCM | April. 2021 - Present
-              </p>
-              <ul>
-                <li>
-                  Developed two web applications for super admins: one to manage
-                  multiple centres, each overseeing classes, permissions, and
-                  users with different roles (staff, instructors, parents, and
-                  students with varying levels), and another for parents to
-                  manage their students, which also including bookings,
-                  absences, and payments. And additionally, a landing page.
-                </li>
-                <li>
-                  Implemented custom reusable components for a responsive user
-                  interface.
-                </li>
-                <li>
-                  Integrated payment processing and discount features for class
-                  bookings.
-                </li>
-                <li>
-                  Enabled multilingual support, allowing the addition of
-                  multiple languages beyond English and Vietnamese.
-                </li>
-                <li>
-                  Created a dynamic dashboard(calendar) displaying real-time
-                  data on class schedules and user activities.
-                </li>
-                <li>
-                  Developed advanced reporting features to track metrics such as
-                  attendance and revenue.
-                </li>
-              </ul>
-            </div>
+            {jobData?.map((jobDt, key) => (
+              <div className="job" key={key}>
+                <div className="headWrap">
+                  <h3>{jobDt.title}</h3>
+                  <p className="location-date">{jobDt.date}</p>
+                </div>
+                <ul>
+                  {jobDt.accomplishments.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
           </section>
 
           <section className="projects">
             <h2>Personal Projects</h2>
-            <div className="project">
-              <h3>The Grappling Network</h3>
-              <ul>
-                <li>Built with GatsbyJS and Styled Components for ReactJS.</li>
-                <li>
-                  Utilized the Reddit API and Google Places API to display
-                  content.
-                </li>
-              </ul>
-            </div>
-            <div className="project">
-              <h3>Jiu Jitsu Tournament Scraper</h3>
-              <ul>
-                <li>Built with NodeJS, Express, MongoDB, and CheerioJS.</li>
-                <li>
-                  Scraped and served multiple Jiu Jitsu tournament results via
-                  API.
-                </li>
-              </ul>
-            </div>
             <div className="project">
               <h3>Portfolio Site</h3>
               <ul>
--- src/const.tsx
+++ src/const.tsx
@@ -34,6 +34,7 @@ export const skills = [
   "WebSockets",
   "Socket io",
   "Web Development",
+  "Recharts",
   "User Experience (UX)",
   "Collaboration",
 ];
@@ -56,6 +57,7 @@ export const highlightedSkills = [
   "Vite",
   "Axios",
   "WebSockets",
+  "Recharts",
 ];
 
 export const name: string = "Le Quoc Bao";
@@ -63,3 +65,32 @@ export const role: string = "Frontend Developer";
 export const email: string = "baole07701@gmail.com";
 export const linkIn: string =
   "https://www.linkedin.com/in/b%E1%BA%A3o-l%C3%AA-1131021a5/";
+
+export const jobData = [
+  {
+    title: "Comprehensive Management Center",
+    date: "Thu Duc, HCM | April 2021 - Present",
+    accomplishments: [
+      "Developed two web applications for super admins: one to manage multiple centres, each overseeing classes, permissions, and users with different roles (staff, instructors, parents, and students with varying levels), and another for parents to manage their students, which also includes bookings, absences, and payments. Additionally, a landing page.",
+      "Implemented custom reusable components for a responsive user interface.",
+      "Integrated payment processing and discount features for class bookings.",
+      "Enabled multilingual support, allowing the addition of multiple languages beyond English and Vietnamese.",
+      "Created a dynamic dashboard displaying real-time data on class schedules and user activities.",
+      "Developed advanced reporting features to track metrics such as attendance and revenue.",
+    ],
+  },
+
+  {
+    title: "NFT Auction Hub",
+    date: "Thu Duc, HCM | October 2021 - January 2023",
+    accomplishments: [
+      "Developed a platform enabling users to buy, bid, and sell NFTs using cryptocurrency, with seamless integration of MetaMask for payments.",
+      "Implemented wallet functionalities, including network switching, token addition, and account details retrieval, enhancing accessibility.",
+      "Built a fully custom UI tailored to the project's unique design requirements using Vue.js and TailwindCSS.",
+      "Designed an automated bidding system with countdown timers, supporting efficient and transparent NFT auctions.",
+      "Integrated user-centric features such as a gallery to view purchased NFTs and options to resell them for cryptocurrency.",
+      "Delivered an interactive onboarding tutorial, guiding users through platform features with step-by-step instructions.",
+      "Ensured a responsive and engaging interface, improving user experience and satisfaction.",
+    ],
+  },
+];
