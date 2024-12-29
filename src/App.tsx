305bb93799ac4f757ec0966aa25b339282384668
lq.bao <lq.bao@tego.global>
2024-12-29 15:55:06 +0700

    Update favicon assests

--- src/App.tsx
+++ src/App.tsx
@@ -126,7 +126,7 @@ const Resume = () => {
               <svg
                 stroke="currentColor"
                 fill="currentColor"
-                stroke-width="0"
+                strokeWidth="0"
                 viewBox="0 0 512 512"
                 height="1em"
                 width="1em"
